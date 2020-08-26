47d05b8a0227dd0c40b688f345e2ccaa509baa8d
itsyeonjoon <41148074+itsyeonjoon@users.noreply.github.com>
2020-08-25 23:19:42 -0700

    Update ConvexHullVisualizer.py

--- src/ConvexHullVisualizer.py
+++ src/ConvexHullVisualizer.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
+import ast
 from ConvexHull import ConvexHull
 
-# test
-listPoints = [[3, 2], [4, 0], [0, -2], [1, 0], [-1, 1], [1, -1], [-1, 0], [-2, 0], [4, -1], [-2, 4], [3, 0], [1, 5], [0, 0], [2, 3]]
+# The following code is a Convex Hull Visualizer using matplotlib.pyplot library. 
+# It iwll receive the points in a form of 2D list, then display the convex hull and its shape. 
+# while all different combination of points will work, try the one I made as a starting point:
+# [[10, 2], [4, 9], [3, 2], [5, 1], [-3, 5], [0, -2], [1, 2], [8, 4], [-1, 1], [5, 5], [1, -1], [-1, 0], [-2, 0], [4, -1], [-2, 4], [3, 0], [1, 5], [0, 0], [2, 3]]
 
+# I will soon try to make the visualizer with better user interaction using GUI in the future :) 
+
+listString = input("Please enter the 2D list of points\nin a form such as the following: ex. [[1, 2], [0, 0], [3, 3], ...]\n")
+listPoints = ast.literal_eval(listString)
 ch = ConvexHull(listPoints)
 ch.sort()
 result = ch.findConvexHull()
